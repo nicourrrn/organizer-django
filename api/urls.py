ae1008e43bcb5bbcb4b6ca1b13460b2926331736
nicourrrn <s57111702@gmail.com>
2023-07-11 13:51:43 +0300

    create viewset and urls handler

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from api import views
+ 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+
+router.register(r'tasks', views.TaskView, basename='task')
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,2 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from api import serializers, models
+
+class TaskView(viewsets.ModelViewSet):
+    queryset = models.Task.objects.all()
+    serializer_class = serializers.TaskSerializer
 
