63f6e05ec32991594b477faea6643d06337e9cd6
nicourrrn <s57111702@gmail.com>
2023-07-11 14:14:46 +0300

    create serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from api import models
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.User
+        fields = "__all__"
+
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Task
@@ -10,3 +15,37 @@ class TaskStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.TaskStatus
         fields = "__all__"
+
+
+class FinOperationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.FinOperation
+        fields = "__all__"
+
+class FinOperationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FinOperationType
+        fields = "__all__"
+
+class FinOperationTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FinOperationTag
+        fields = "__all__"
+
+class NoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Note
+        fields = "__all__"
+
+class NoteTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.NoteType
+        fields = "__all__"
+
+
+class NoteTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.NoteTag
+        fields = "__all__"
+
+
