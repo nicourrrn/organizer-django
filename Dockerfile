dc502048fa945890ae6886e5362e5e7fdab9cb65
nicourrrn <s57111702@gmail.com>
2023-07-11 22:03:40 +0300

    Add docker support

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN pacman -Sy python-poetry --noconfirm
 WORKDIR /app
 COPY . .
 RUN poetry install
-CMD ["poetry", "run", "uvicorn", "fin_manager.asgi:application"]
+CMD ["poetry", "run", "uvicorn", "--host", "0.0.0.0", "fin_manager.asgi:application"]
 
--- fin_manager/settings.py
+++ fin_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'corsheaders',
     'api',
 ]
 
@@ -49,6 +50,7 @@ AUTH_USER_MODEL = 'api.Client'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
